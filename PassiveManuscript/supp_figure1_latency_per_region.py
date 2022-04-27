ed92a3c8206e7b5c9d6d9775d7ae9b93b2b9be79
Guido Meijer <gtmeijer@gmail.com>
2022-04-27 12:42:02 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ PassiveManuscript/supp_figure1_latency_per_region.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 14 15:19:58 2021
+By: Guido Meijer
+"""
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from os.path import join
+from serotonin_functions import (paths, figure_style, load_subjects, plot_scalar_on_slice,
+                                 combine_regions)
+
+# Settings
+MIN_NEURONS = 20
+MIN_PERC = 8
+
+# Paths
+fig_path, save_path = paths(dropbox=True)
+fig_path = join(fig_path, 'PaperPassive')
+
+# Load in results
+all_neurons = pd.read_csv(join(save_path, 'light_modulated_neurons.csv'))
+all_neurons['full_region'] = combine_regions(all_neurons['region'], split_thalamus=False)
+
+# Add genotype
+subjects = load_subjects()
+for i, nickname in enumerate(np.unique(subjects['subject'])):
+    all_neurons.loc[all_neurons['subject'] == nickname, 'sert-cre'] = subjects.loc[subjects['subject'] == nickname, 'sert-cre'].values[0]
+
+# Only sert-cre mice
+sert_neurons = all_neurons[all_neurons['sert-cre'] == 1]
+
+# Transform to ms
+sert_neurons['latency'] = sert_neurons['latency_peak_hw'] 
+
+# Get percentage modulated per region
+reg_neurons = sert_neurons.groupby('full_region').median()['latency'].to_frame()
+reg_neurons['n_neurons'] = sert_neurons.groupby(['full_region']).size()
+reg_neurons['perc_mod'] = (sert_neurons.groupby(['full_region']).sum()['modulated']
+                           / sert_neurons.groupby(['full_region']).size()) * 100
+reg_neurons = reg_neurons.loc[reg_neurons['n_neurons'] >= MIN_NEURONS]
+reg_neurons = reg_neurons.reset_index()
+reg_neurons = reg_neurons[reg_neurons['full_region'] != 'root']
+reg_neurons = reg_neurons[reg_neurons['n_neurons'] >= MIN_NEURONS]
+reg_neurons = reg_neurons[reg_neurons['perc_mod'] >= MIN_PERC]
+reg_neurons = reg_neurons.sort_values('latency')
+
+# Apply selection criteria
+sert_neurons = sert_neurons[sert_neurons['full_region'].isin(reg_neurons['full_region'])]
+
+# Order regions
+ordered_regions = sert_neurons.groupby('full_region').median().sort_values('latency', ascending=True).reset_index()
+
+# %%
+
+colors, dpi = figure_style()
+f, ax1 = plt.subplots(1, 1, figsize=(3.5, 2.2), dpi=dpi)
+#sns.pointplot(x='latency', y='full_region', data=sert_neurons, order=ordered_regions['full_region'],
+#              join=False, ci=68, color=colors['general'], ax=ax1)
+sns.boxplot(x='latency', y='full_region', data=sert_neurons, order=ordered_regions['full_region'],
+            color=colors['general'], fliersize=0, linewidth=0.75, ax=ax1)
+ax1.set(xlim=[-0.01, 1.01], xlabel='Modulation onset latency (s)', ylabel='')
+#plt.xticks(rotation=90)
+plt.tight_layout()
+sns.despine(trim=True, offset=3)
+plt.savefig(join(fig_path, 'figure4_modulation_latency_per_region.pdf'))
+
---
+++ PassiveManuscript/supp_figure2_GLM_motion.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  6 09:25:11 2022
+By: Guido Meijer
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from os.path import join
+from scipy.stats import mannwhitneyu
+from serotonin_functions import paths, load_subjects, remap, figure_style, combine_regions
+
+# Initialize some things
+FIG = 2
+MIN_NEURONS = 10
+MOTION_REG = ['wheel_velocity', 'nose', 'paw_l', 'paw_r', 'tongue_end_l', 'tongue_end_r',
+              'motion_energy_body', 'motion_energy_left', 'motion_energy_right', 'pupil_diameter']
+fig_path, save_path = paths(dropbox=True)
+fig_path = join(fig_path, 'PaperPassive')
+subjects = load_subjects()
+
+# Load in GLM output
+all_glm_df = pd.read_csv(join(save_path, 'GLM', 'GLM_passive_opto.csv'))
+
+# Add sert-cre
+for i, nickname in enumerate(np.unique(subjects['subject'])):
+    all_glm_df.loc[all_glm_df['subject'] == nickname, 'sert-cre'] = subjects.loc[
+        subjects['subject'] == nickname, 'sert-cre'].values[0]
+    
+# Add regions
+all_glm_df['region'] = remap(all_glm_df['acronym'])
+all_glm_df['full_region'] = combine_regions(all_glm_df['region'])
+
+# Drop root
+all_glm_df = all_glm_df[all_glm_df['region'] != 'root']
+
+# Set 0 regressors to NaN
+all_glm_df.loc[all_glm_df['motion_energy_left'] < 0.00001, 'motion_energy_left'] = np.nan
+all_glm_df.loc[all_glm_df['motion_energy_right'] < 0.00001, 'motion_energy_right'] = np.nan
+all_glm_df.loc[all_glm_df['motion_energy_body'] < 0.00001, 'motion_energy_body'] = np.nan
+
+# Get max motion regressor
+all_glm_df['max_motion'] = all_glm_df[MOTION_REG].max(axis=1)
+
+# %%
+
+# Drop root and only keep modulated neurons
+glm_df_slice = all_glm_df[(all_glm_df['sert-cre'] == 1) & (all_glm_df['full_region'] != 'root')]
+grouped_df = glm_df_slice.groupby('full_region').size()
+grouped_df = grouped_df[grouped_df >= MIN_NEURONS]
+glm_df_slice = glm_df_slice[glm_df_slice['full_region'].isin(grouped_df.index.values)]
+
+sort_regions = glm_df_slice.groupby('full_region').mean()['max_motion'].sort_values(
+    ascending=False).index.values
+
+colors, dpi = figure_style()
+f, ax1 = plt.subplots(1, 1, figsize=(2.5, 1.75), dpi=dpi)
+sns.boxplot(x='max_motion', y='full_region', color='orange', ax=ax1,
+            data=glm_df_slice, order=sort_regions, fliersize=0)
+ax1.set(ylabel='', xlabel=u'Δ var. explained by motion', xlim=[0, 0.3],
+        xticks=[0, .1, .2, .3])
+
+plt.tight_layout()
+sns.despine(trim=True, offset=2)
+plt.savefig(join(fig_path, f'supp_figure{FIG}_GLM_motion_per_region.pdf'))
